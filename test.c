ab989fe6a3267d667d88044e5456aa7a490baca0
HuzaifaSu <msumai24@student.aau.dk>
2024-11-10 18:49:03 +0100

    Update test.c

--- test.c
+++ test.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-
      double a, b;
 
-     printf("Write a %d", a, b);
-     scanf("%d", &a, &b);
+     printf("Write an (a) value: ");
+     scanf("%lf", &a);
 
+     printf("Write an (b) value: ");
+     scanf("%lf", &b);
+
+     if (a < b)
+     {
+          printf("a: %lf is less then b: %dlf", a, b);
+     }
+     else if(a == b)
+     {
+          printf("a: %lf is equal to b: %lf", a,b);
+     }else
+     {
+          printf("a: %lf is bigger then b: %lf", a,b);
+     }
+     
+     
 
      return 0;
 }
